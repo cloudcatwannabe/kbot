93b4ccdbd72834255ffdb28de57ccfaca6eda81a
Lino Espinoza <admin@linoespinoza.com>
2020-08-26 12:30:58 +0000

    Slice 4: Se agrega el calculo del impuesto, hardcodeado

--- factura.rb
+++ factura.rb
@@ -5,14 +5,22 @@
 # rgemuby factura.rb <cantidad> <precio unitario> <estado>
 
 class Billing
-  def calculate_price(quantity, price)
+  def calculate_price(quantity, price, state)
     subtotal = quantity.to_i * price.to_f
+    tax_rate = 0
+    
+    if state == 'CA'
+      tax_rate = 0.0825
+    end
+
+    tax = subtotal * tax_rate;
+    total = subtotal + tax;
     puts "Sub Total: $#{subtotal}"
-    puts "Tax: 0"
+    puts "Tax: $#{tax}"
     puts "Discount: 0"
-    puts "Total: 0"
+    puts "Total: $#{total}"
   end
 end
 
 bill = Billing.new
-puts bill.calculate_price(ARGV[0], ARGV[1])
+puts bill.calculate_price(ARGV[0], ARGV[1], ARGV[2])
