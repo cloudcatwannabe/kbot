c0736ac6cd874c99b32a26357afe068f3e95fd51
linoespinoza-ipad <woztheman+ipad@gmail.com>
2020-08-25 21:59:21 -0500

    Inicial

--- factura.rb
+++ factura.rb
@@ -1,1 +1,8 @@
+# L24/08 - CSD Kleer
+# Construir una aplicación de línea de comandos que permita calcular los datos de facturación 
+# incluyendo impuestos y descuentos.
+# Debe poder ejecutarse con la línea de comando:
+# rgemuby factura.rb <cantidad> <precio unitario> <estado>
+
+
 puts "Hola, mundo"
