ca59bfbf8041667f1da6f3ccd2a1a2f7401e4df5
Lino Espinoza <admin@linoespinoza.com>
2020-08-26 12:38:47 +0000

    Slice 5: Se mejora el calculo del impuesto por estado

--- factura.rb
+++ factura.rb
@@ -5,19 +5,24 @@
 # rgemuby factura.rb <cantidad> <precio unitario> <estado>
 
 class Billing
+  def initialize
+    @taxes = {
+      'UT' => 0.0685,
+      'NV' => 0.080,
+      'TX' => 0.0625,
+      'AL' => 0.040,
+      'CA' => 0.0825
+    }
+  end
+
   def calculate_price(quantity, price, state)
     subtotal = quantity.to_i * price.to_f
-    tax_rate = 0
-    
-    if state == 'CA'
-      tax_rate = 0.0825
-    end
-
-    tax = subtotal * tax_rate;
-    total = subtotal + tax;
+    tax_rate = @taxes[state.upcase]
+    tax = subtotal * tax_rate
+    total = subtotal + tax
     puts "Sub Total: $#{subtotal}"
     puts "Tax: $#{tax}"
-    puts "Discount: 0"
+    puts 'Discount: $0'
     puts "Total: $#{total}"
   end
 end
